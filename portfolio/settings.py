995537f30cfd8d3f165b4181f9aead39e22f034c
kk <kk41110002@gmail.com>
2021-03-06 23:33:21 +0530

    deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR,]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
