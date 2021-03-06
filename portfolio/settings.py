6f692026c163bbb7168a958c835a37be22a21716
kk <kk41110002@gmail.com>
2021-03-07 00:37:37 +0530

    deleting

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR,]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
