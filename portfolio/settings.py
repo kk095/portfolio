a11c06675fe9e0600419d94b3557e21a18378c70
kk <kk41110002@gmail.com>
2021-05-21 09:13:33 +0530

    adding config var

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,4 +118,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kkresume095@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ilove$0n!'
+EMAIL_HOST_PASSWORD = os.environ['password']
